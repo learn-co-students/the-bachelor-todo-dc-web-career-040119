63f43195f6b7952e80f001af6e3585e0019ad173
mario rodrigueza <mariorodriguezan@gmail.com>
2019-04-01 00:58:37 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,51 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+    data[season].each do |people|
+      people.each do |category, result|
+        if result == "Winner"
+          return people["name"].split(' ').first
+        end
+      end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |person|
+      if person["occupation"] == occupation
+        return person["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end # code here
 end
 
+
+
 def get_average_age_for_season(data, season)
-  # code here
+  total = 0
+  data[season].each do |contestant|
+    total += contestant["age"].to_f
+  end
+  (total / data[season].count).round # code here
 end
